236e39ac0e8f05bfb2ff5da60f7132726749d788
Evgenyi <evgenyi.logachev@gmail.com>
2026-04-13 19:30:27 +0300

    130426

---
+++ estimate_M_from_clean_peaks.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+from scipy.signal import find_peaks
+
+def estimate_M_from_clean_peaks(data:pd.DataFrame,min_height:int=200, 
+                         min_distance:int=10, min_purity:float=0.5 ,eps=1e-12):
+    """
+    peaks_by_dye: dict {0:[peak1, peak2, ...], 1:[...], 2:[...], 3:[...]}
+    Каждый peak = array-like shape (4,), порядок каналов один и тот же
+    Возвращает M shape (4,4), где столбец j соответствует красителю j.
+    """
+    M = np.zeros((4, 4), dtype=float)
+    data=data.values
+    
+    
+    # --- Найти все пики (один раз) ---
+    envelope = data.max(axis=1)
+    peak_pos, _ = find_peaks(envelope, height=min_height, 
+                              distance=min_distance)
+    peak_I = data[peak_pos, :]  # (N_peaks, 4)
+    
+    # Нормируем пики для M-шага
+    norms = peak_I.sum(axis=1, keepdims=True)
+    norms[norms == 0] = 1
+    peak_normalized = peak_I / norms
+    top_indices = np.argsort(peak_I.sum(axis=1))[-4:]  # индексы 4 самых ярких пиков
+    peaks_by_dye = peak_normalized[top_indices]  # (4, 4)
+
+    for j in range(4):
+        peaks = peaks_by_dye[j,:]
+        Y = []
+        W = []
+
+        for x in peaks:
+            x = np.asarray(x, dtype=float)
+            s = x.sum()
+            if s <= eps:
+                continue
+            y = x / s
+            Y.append(y)
+            W.append(s)   # вес: общая интенсивность пика
+
+        if len(Y) == 0:
+            M[:, j] = np.eye(4)[:, j]
+            continue
+
+        Y = np.vstack(Y)              # (n_peaks, 4)
+        W = np.asarray(W)[:, None]    # (n_peaks, 1)
+
+        m = (W * Y).sum(axis=0) / W.sum()
+        m = np.clip(m, 0, None)
+        m_sum = m.sum()
+        if m_sum > eps:
+            m /= m_sum
+        else:
+            m = np.eye(4)[:, j]
+
+        M[:, j] = m
+
+    return M
